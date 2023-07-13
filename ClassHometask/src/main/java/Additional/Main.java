4d00a59890979d222f9d02bb350af13e32baea21
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-07-13 19:47:55 +0300

    Added new computer and added EXTERNAL enum element

--- ClassHometask/src/main/java/Additional/Main.java
+++ ClassHometask/src/main/java/Additional/Main.java
@@ -4,11 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Computer computer1 = new Computer(1500, "Dota2PC");
         Computer computer2 = new Computer(2500, "Dota2PROpc", new HDD("ScytheOfVyse", 500, TypeOfExterniti.INTERNAL), new RAM("BootsOfTravel", 16));
+        Computer computer3 = new Computer(2500, "Dota2PROpcMega", new HDD("EyeOfScadi", 1000, TypeOfExterniti.EXTERNAL), new RAM("ScrollOfTeleportation", 64));
+
 
         computer1.toString();
         computer2.toString();
+        computer3.toString();
 
         System.out.println(computer1);
         System.out.println(computer2);
+        System.out.println(computer3);
     }
 }
