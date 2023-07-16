13472076826cd31a40e28115eb0a1188db3a3d17
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-07-17 00:13:07 +0300

    fixed some issues

---
+++ Interfaces_and_abstract_hometask/src/main/java/AdditionalTask/Docs/FinancialInvoice.java
@@ -0,0 +1,20 @@
+package AdditionalTask.Docs;
+
+import AdditionalTask.Service.Document;
+
+import java.util.Date;
+
+public class FinancialInvoice extends Document {
+    private int codeOfDepartment;
+    private int finalMonthSum;
+
+    public FinancialInvoice(Date dateOfDocument, int numberOfDocument, int codeOfDepartment, int finalMonthSum) {
+        super(dateOfDocument, numberOfDocument);
+        this.codeOfDepartment = codeOfDepartment;
+        this.finalMonthSum = finalMonthSum;
+    }
+    @Override
+    public void infoAboutDoc() {
+        System.out.println("Документ №" + numberOfDocument + "\n" + "Сумма за месяц: " + finalMonthSum + "\n" + "Код департамента: " + codeOfDepartment + "\n" + "Дата: " + dateOfDocument);
+    }
+}
