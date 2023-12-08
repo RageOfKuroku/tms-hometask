590e9f7f7a69141356ba9253c1446b0359d4d67d
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-12-08 21:33:52 +0300

    main files

---
+++ RestSecondModule/src/main/java/com/example/restsecondmodule/RestSecondModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.restsecondmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSecondModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSecondModuleApplication.class, args);
+    }
+
+}
