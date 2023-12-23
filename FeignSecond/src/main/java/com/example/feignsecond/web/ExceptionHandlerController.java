5dc100629dc568a050a4e1a0879e3f4860748cbc
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-12-23 15:55:56 +0300

    Swagger added

--- FeignSecond/src/main/java/com/example/feignsecond/web/ExceptionHandlerController.java
+++ FeignSecond/src/main/java/com/example/feignsecond/web/ExceptionHandlerController.java
@@ -3,19 +3,24 @@ package com.example.feignsecond.web;
 import com.example.feignsecond.dto.IlErrorDto;
 import com.example.feignsecond.exception.FailedTaskException;
 import com.example.feignsecond.mapper.ErrorMapper;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RequiredArgsConstructor
 @RestControllerAdvice
+@OpenAPIDefinition(info = @Info(
+        title = "Controller for handling errors"
+))
 public class ExceptionHandlerController {
 
     private final ErrorMapper mapper;
     @ExceptionHandler(FailedTaskException.class)
+    @Operation(tags = "handlers", description = "Too lazy for filling description")
     public ResponseEntity<IlErrorDto> handle(FailedTaskException exception) {
-
-
         return ResponseEntity.status(exception.getCode()).body(mapper.toIl(exception));
     }
 }
